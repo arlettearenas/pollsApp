3ad83721132645af960d5703dd5d6d1f54be8cc1
arlettearenas <arlettearenas29@gmail.com>
2024-05-27 13:51:13 -0600

    03 Implements votes in form vote

--- poll/views.py
+++ poll/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import Question
 
 # Create( your views here.
@@ -13,10 +13,23 @@ def home(request):
 
 
 def vote(request, q_id):
-    q = Question.objects.get(pk=q_id)
+    q = get_object_or_404(Question, pk=q_id)
+    if request.method == "POST":
+        choice_id = request.POST.get('choice')
+        choice = q.choice_set.get(pk=choice_id)
+        choice.votes += 1
+        choice.save()
+        return redirect('poll:result', q_id)
     return render(request, 'poll/vote.html', {
         "question": q
     })
 
 def result(request, q_id):
-    pass 
+    try:
+        q = Question.objects.get(pk=q_id)
+    except Question.DoesNotExist:
+        return redirect('poll:home')
+    return render(request, 'poll/result.html', {
+        "question": q
+    })
+
