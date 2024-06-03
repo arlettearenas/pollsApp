1717b4b6c533553ebc6218510b82ccb8721b70fd
arlettearenas <arlettearenas29@gmail.com>
2024-06-03 14:24:33 -0600

    04 Creación de gráfica

--- poll/views.py
+++ poll/views.py
@@ -1,35 +1,41 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from .models import Question
+from .models import Question, Choice
+import json
 
-# Create( your views here.
 def home(request):
     questions = Question.objects.all()
-    return render(
-        request,
-        'poll/home.html',
-        {
-            "questions":questions
-        })
-
+    return render(request, 'poll/home.html', {
+        "questions": questions
+    })
 
 def vote(request, q_id):
     q = get_object_or_404(Question, pk=q_id)
     if request.method == "POST":
-        choice_id = request.POST.get('choice')
-        choice = q.choice_set.get(pk=choice_id)
-        choice.votes += 1
-        choice.save()
-        return redirect('poll:result', q_id)
+        try:
+            choice_id = request.POST.get('choice')
+            choice = q.choice_set.get(pk=choice_id)
+            choice.votes += 1
+            choice.save()
+            return redirect('poll:result', q_id)
+        except (KeyError, Choice.DoesNotExist):
+            return render(request, 'poll/vote.html', {
+                "question": q,
+                "error_message": "Debes de elegir algo!! XDXDXDXDXD"
+            })
     return render(request, 'poll/vote.html', {
         "question": q
     })
 
 def result(request, q_id):
-    try:
-        q = Question.objects.get(pk=q_id)
-    except Question.DoesNotExist:
-        return redirect('poll:home')
-    return render(request, 'poll/result.html', {
-        "question": q
-    })
+    q = get_object_or_404(Question, pk=q_id)
+    choices = q.choice_set.all()
+    choice_texts = json.dumps([choice.choice_text for choice in choices])
+    votes = json.dumps([choice.votes for choice in choices])
 
+    print(choice_texts, votes)  
+
+    return render(request, 'poll/result.html', {
+        "question": q,
+        "choice_texts": choice_texts,
+        "votes": votes
+    })
